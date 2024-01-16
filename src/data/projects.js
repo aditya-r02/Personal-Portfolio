1c924ab5df103185bae16be608bb3b3efbce9cff
Aditya Rana <ranaaditya01@outlook.com>
2024-01-16 14:30:28 +0530

    projects added

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,6 +7,12 @@ export const data = [
         "link":"https://github.com/aditya-r02/StudyNotion"
     },
     {
+        "heading":"Sudoku Solver",
+        "stack":"Backtracking, HTML, CSS, Javascript, ReactJS",
+        "url":"https://res.cloudinary.com/dqdy8u03v/image/upload/v1705395532/Sudoku_sequtr.png",
+        "link":"https://sudoku-solver-by-adityarana.vercel.app/"
+    },
+    {
         "heading":"College Clubs",
         "stack":"HTML, CSS, Javascript, ReactJS",
         "url":"https://res.cloudinary.com/dqdy8u03v/image/upload/v1702746104/collegeclubs_xhy68g.png",
